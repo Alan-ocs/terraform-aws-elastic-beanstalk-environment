a2efc9289a2a8e3e37bc5b7b0e381e970fc7b7bc
Alan Silva <80364531+Alan-ocs@users.noreply.github.com>
2024-04-27 20:32:09 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,14 +3,14 @@ resource "aws_elastic_beanstalk_environment" "default" {
   application             = var.app_name
   solution_stack_name     = var.solution_stack
   wait_for_ready_timeout  = var.timeout
-### ENVIRONMENT SETTINGS  ###
-  dynamic setting {
+
+  # ENVIRONMENT SETTINGS
+  dynamic "setting" {
     for_each = var.env_settings
     content {
-      namespace = setting.value[0]
-      name      = setting.value[1]
-      value     = setting.value[2]
+      namespace = setting.value["namespace"]
+      name      = setting.value["name"]
+      value     = setting.value["value"]
     }
   }
 }
-
