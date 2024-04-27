975c41c5992fdae19108a5fa75606ad5be19511c
Alan Silva <80364531+Alan-ocs@users.noreply.github.com>
2024-04-27 20:34:34 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,57 @@
-resource "aws_elastic_beanstalk_environment" "default" {
-  name                    = var.env_name
-  application             = var.app_name
-  solution_stack_name     = var.solution_stack
-  wait_for_ready_timeout  = var.timeout
+variable "my_app_env_name" {
+  description = "EB environment name"
+  type        = string
+}
 
-  # ENVIRONMENT SETTINGS
-  dynamic "setting" {
-    for_each = var.env_settings
-    content {
-      namespace = setting.value["namespace"]
-      name      = setting.value["name"]
-      value     = setting.value["value"]
-    }
-  }
+variable "my_app_application" {
+  description = "EB application name"
+  type        = string
+}
+
+variable "my_app_env_settings" {
+  description = "A list of environment settings for transaction broker"
+  type        = list(object({
+    namespace = string
+    name      = string
+    value     = string
+  }))
+}
+
+variable "my_app_solution_stack" {
+  description = "Solution stack for environment"
+  type        = string
+}
+
+variable "my_app_timeout" {
+  description = "EB environment deploy timeout"
+  type        = string
+}
+
+variable "env_tags" {
+  description = "Environment tags"
+  type        = map(string)
+}
+
+variable "tags" {
+  description = "Common tags for resources"
+  type        = map(string)
+}
+
+provider "aws" {
+  region  = "us-west-2"
+  profile = var.env_tags["environment"]
+}
+
+module "my_app_environment" {
+  source          = "github.com/paul-lupu/tf-eb-environment.git"
+  env_name        = var.my_app_env_name
+  app_name        = var.my_app_application
+  solution_stack  = var.my_app_solution_stack
+  timeout         = var.my_app_timeout
+  env_settings    = var.my_app_env_settings
+  tags            = local.tags
+}
+
+locals {
+  tags = merge(var.tags, var.env_tags)
 }
