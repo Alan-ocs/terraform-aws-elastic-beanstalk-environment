fd3d356a914210cdbbb42fbee87c4db6e9d34b77
Alan Silva <80364531+Alan-ocs@users.noreply.github.com>
2024-04-27 20:32:37 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,28 +1,41 @@
 variable "timeout" {
-  description = "Deploy timeout" 
+  description = "Deploy timeout"
+  type        = string
   default     = "20m"
 }
 
 variable "env_settings" {
-  type        = "list"
-  description = "A list of 3 item lists in the format env_settings = [ [namespace, name, value] ]"
-  default     = [ 
-    [ "aws:elasticbeanstalk:application:environment", "BUNDLE_WITHOUT", "test:development"]
-  ]
+  description = "A map of environment settings"
+  type        = map(object({
+    namespace = string
+    name      = string
+    value     = string
+  }))
+  default     = {
+    BUNDLE_WITHOUT = {
+      namespace = "aws:elasticbeanstalk:application:environment"
+      name      = "BUNDLE_WITHOUT"
+      value     = "test:development"
+    }
+  }
 }
 
-variable "app_name" { 
+variable "app_name" {
   description = "The name of the application to which to attach the environment"
+  type        = string
 }
 
 variable "env_name" {
   description = "Name for elastic beanstalk environment"
+  type        = string
 }
 
 variable "solution_stack" {
   description = "Elastic beanstalk environment solution stack"
+  type        = string
 }
 
-variable "tags" { 
-  type        = "map"
+variable "tags" {
+  description = "A map of tags"
+  type        = map(string)
 }
